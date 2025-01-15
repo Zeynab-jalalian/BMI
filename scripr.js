0c80e2f9ecc98646f0bd6f3fc3596c7f1f0c1665
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-15 22:42:43 +0330

    fix bug

--- scripr.js
+++ scripr.js
@@ -2,6 +2,7 @@ let form=document.getElementById('bmi-form'); //دسترسی به فرم
 let button=document.querySelector('button'); //دسترسی به دکمه ی محاسبه
 let bmiValue=document.getElementById('bmi-value');
 let interpretation=document.getElementById('interpretation');
+let text=document.getElementById('text');
 
 button.addEventListener('click',function(event){ //عملیات کلیک
     event.preventDefault(); //جلوگیری از رفرش شدن صفحه
@@ -13,11 +14,15 @@ button.addEventListener('click',function(event){ //عملیات کلیک
     
     if(BMI<18.5){
         interpretation.innerHTML="تفسیر : کم وزن";
-    }else if(BMI>=18.5 && BMI<=24.9){
+        text.innerHTML="ت باید یه لقمه بیشتر بخوری، شاید بتونی به طرف دیگه قابلمه برسی!"
+    }else if(BMI>=18.5 && BMI<=24.99){
         interpretation.innerHTML="تفسیر : وزن نرمال";
-    }else if(BMI>=25 && BMI<=29.9){
+        text.innerHTML="نه خیلی چاق، نه خیلی لاغر… مثل یک کیسه برنج متوسط! نه سنگین، نه سبک، فقط در بهترین حالت خودت!";
+    }else if(BMI>=25 && BMI<=29.99){
         interpretation.innerHTML="تفسیر :اضافه وزن";
+        text.innerHTML="مثل یه کوله‌پشتی پر از خاطرات خوش، فقط یادت باشه گاهی وقت‌ها باید کمی از بارش کم کنی تا راحت‌تر حرکت کنی!";
     }else{
-        interpretation.innerHTML="تفسیر : چاق";
+        interpretation.innerHTML="تفسیر : وزن بالا";
+        text.innerHTML="باید از سنگینی انتخاب‌هایت لذت ببری، ولی یه پیاده‌روی تو هوای آزاد شاید کمکت کنه!";
     }
 })
