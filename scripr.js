472a7e74f759c8aea64be10849126e99fc6376ec
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-15 20:43:49 +0330

    js step 3

--- scripr.js
+++ scripr.js
@@ -1,12 +1,13 @@
-let form=document.getElementById('bmi-form');
-let button=document.querySelector('button');
+let form=document.getElementById('bmi-form'); //دسترسی به فرم
+let button=document.querySelector('button'); //دسترسی به دکمه ی محاسبه
+let bmiValue=document.getElementById('bmi-value');
 
-button.addEventListener('click',function(event){
-    event.preventDefault();
-    let weight=parseFloat(document.getElementById('weight').value);
-    let height=parseFloat(document.getElementById('height').value);
-    let h=height/100;
-    let BMI=weight/(h**2);
-    console.log(BMI);
+button.addEventListener('click',function(event){ //عملیات کلیک
+    event.preventDefault(); //جلوگیری از رفرش شدن صفحه
+    let weight=parseFloat(document.getElementById('weight').value); //مقدار وزن
+    let height=parseFloat(document.getElementById('height').value); //مقدار قد
+    let h=height/100; //مقدار ارتفاع به متر
+    let BMI=weight/(h**2); //فرمول BMI
+    bmiValue.innerHTML=`BMI = ${BMI.toFixed(2)}`;
     
 })
