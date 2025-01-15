3e55ae01cb4b6d5eb4e65f5fd411f9aecce7f61b
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-15 20:35:24 +0330

    js step2

--- scripr.js
+++ scripr.js
@@ -1,8 +1,12 @@
 let form=document.getElementById('bmi-form');
 let button=document.querySelector('button');
-let weight=document.getElementById('weight');
-let height=document.getElementById('height');
 
 button.addEventListener('click',function(event){
     event.preventDefault();
+    let weight=parseFloat(document.getElementById('weight').value);
+    let height=parseFloat(document.getElementById('height').value);
+    let h=height/100;
+    let BMI=weight/(h**2);
+    console.log(BMI);
+    
 })
