f263c4f81320335645712dd4a3844353362c8b49
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-15 21:21:35 +0330

    js

--- scripr.js
+++ scripr.js
@@ -1,6 +1,7 @@
 let form=document.getElementById('bmi-form'); //دسترسی به فرم
 let button=document.querySelector('button'); //دسترسی به دکمه ی محاسبه
 let bmiValue=document.getElementById('bmi-value');
+let interpretation=document.getElementById('interpretation');
 
 button.addEventListener('click',function(event){ //عملیات کلیک
     event.preventDefault(); //جلوگیری از رفرش شدن صفحه
@@ -10,4 +11,13 @@ button.addEventListener('click',function(event){ //عملیات کلیک
     let BMI=weight/(h**2); //فرمول BMI
     bmiValue.innerHTML=`BMI = ${BMI.toFixed(2)}`;
     
+    if(BMI<18.5){
+        interpretation.innerHTML="تفسیر : کم وزن";
+    }else if(BMI>=18.5 && BMI<=24.9){
+        interpretation.innerHTML="تفسیر : وزن نرمال";
+    }else if(BMI>=25 && BMI<=29.9){
+        interpretation.innerHTML="تفسیر :اضافه وزن";
+    }else{
+        interpretation.innerHTML="تفسیر : چاق";
+    }
 })
